f2634dbf49a51a1d6a57efe1c3cc5156a4855692
cmarion-mq <cmarion.mq@gmail.com>
2021-05-18 19:08:01 +0200

    mail-debranchement email2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Eventbrite_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  
+  #config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
