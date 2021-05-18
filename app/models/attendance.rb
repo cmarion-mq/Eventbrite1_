827e7862887bfd25ff0cb254ac524435db519459
cmarion-mq <cmarion.mq@gmail.com>
2021-05-18 08:08:00 +0200

    end J1

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,10 @@
 class Attendance < ApplicationRecord
   belongs_to :event
   belongs_to :attendee, class_name: "User"
+
+  after_create :new_attendee_email_send
+
+  def new_attendee_email_send
+    UserMailer.new_attendee_email(self).deliver_now
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_many :events, foreign_key: "administrator_id"
   has_many :attendances, foreign_key: "attendee_id"
+
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
